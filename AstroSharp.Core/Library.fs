5c2bd6195bc70c250150d9d44986dcd5827b8149
Kevin Peters <kwpeters@ra.rockwell.com>
2021-01-28 09:14:26 -0500

    Added a function and tests.

--- AstroSharp.Core/Library.fs
+++ AstroSharp.Core/Library.fs
@@ -1,2 +1,8 @@
 ﻿namespace AstroSharp.Core
 
+module Gravitation =
+    let G = 6.67408 * ( 10.0 ** -11.0 )
+    let getGravitationForce m1 m2 r =
+        if r = 0.0 || m1 < 0.0 || m2 < 0.0 then None
+        else
+            Some ( G * m1 * m2 / ( r * r ))
--- AstroSharp.Test/Program.fs
+++ AstroSharp.Test/Program.fs
@@ -1,8 +1,31 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Expecto
+open AstroSharp.Core.Gravitation
+
+
+let gravitationTests =
+    testList "Gravitation Tests" [
+        test "0 Distance Between Bodies" {
+            let noneResult = getGravitationForce 10.0 10.0 0.0
+            Expect.equal noneResult None "Result is None since the distance is 0"
+        }
+        test "Mass of Body 1 is Negative" {
+            let noneResult = getGravitationForce -1.0 10.0 0.0
+            Expect.equal noneResult None "Result is None since the mass of the 1st body is negative"
+        }
+        test "Mass of Body 2 is Negative" {
+            let noneResult = getGravitationForce 10.0 -1.0 0.0
+            Expect.equal noneResult None "Result is None since the mass of the 2nd body is negative"
+        }
+        test "Gravitation Smoke Test" {
+            let someResult = getGravitationForce 10.0 10.0 1.0
+            Expect.isSome someResult "Result has a Value i.e. not None"
+            Expect.floatClose Accuracy.medium someResult.Value ( G * 100.0 ) "Result is 100 times G"
+        }
+    ]
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    runTestsWithArgs defaultConfig argv gravitationTests
